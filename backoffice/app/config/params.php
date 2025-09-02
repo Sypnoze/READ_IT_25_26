d3b256fe32f2fddff5d2c53bba5a7d68deaec607
Sypnoze <onurkucukkaya.webdev@gmail.com>
2025-09-02 11:47:20 +0200

    Structure publicoffice + backoffice

---
+++ backoffice/app/config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+//Paramètre de connexion
+
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '8889');
+define('DB_NAME', 'readit_2020');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
